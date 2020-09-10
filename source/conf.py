4373ac59cde7af3e06ee32ab3e3c5557e05ffc88
ardaci <dongjie.shi@intel.com>
2020-09-10 02:43:00 +0000

    update

--- source/conf.py
+++ source/conf.py
@@ -116,8 +116,7 @@ language = None
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 
-exclude_patterns = ['_build']
-exclude_patterns += sphinx_gallery_conf['examples_dirs']
+exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
